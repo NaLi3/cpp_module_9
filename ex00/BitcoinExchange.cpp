69ba5a4d92abc564fd122206b67dafd19a767650
Ilan Levy <ilevy@f2r4s20.clusters.42paris.fr>
2025-07-07 10:11:38 +0200

    started 00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,135 @@
 /*   By: ilevy <ilevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 15:18:05 by ilevy             #+#    #+#             */
-/*   Updated: 2025/06/27 15:18:06 by ilevy            ###   ########.fr       */
+/*   Updated: 2025/07/07 10:10:16 by ilevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+
+// Constructors and destructor
+BitcoinExchange::BitcoinExchange( void )
+{
+	std::cout << "Non instantiable without parameters" << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange( const std::string& inputName ): _inputName(inputName)
+{
+	std::cout << "Parameterized constructor called with input file path: " << this->getInputName() << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange( const BitcoinExchange& copy ): _inputName(copy.getInputName()), _exchangeRates(copy.getExchangeRates())
+{
+	std::cout << "Copy constructor called with copy input file path: " << copy.getInputName() << std::endl;
+}
+
+BitcoinExchange& BitcoinExchange::operator=( const BitcoinExchange& src )
+{
+	if (this != &src )
+	{
+		this->_exchangeRates = src.getExchangeRates();
+		this->_inputName = src.getInputName();
+	}
+	
+	return (*this);
+}
+
+BitcoinExchange::~BitcoinExchange( void )
+{
+	std::cout << RED << "Default destructor called" << RESET << std::endl;
+}
+
+// Getter functions
+const std::map<std::string, double>&	BitcoinExchange::getExchangeRates( void ) const
+{
+	return (this->_exchangeRates);
+}
+
+const std::string&						BitcoinExchange::getInputName( void ) const
+{
+	return (this->_inputName);
+}
+
+//Loads database in ifstream variable, checks its opening and then sends to parsing inner function
+int	BitcoinExchange::loadDatabase( const std::string& databaseName )
+{
+	std::ifstream file;
+
+	file.open(databaseName);
+	if (!file.is_open())
+	{
+		std::cerr << RED << "Error: Could not open database file with path: " << RESET << databaseName << std::endl;
+		return (ERROR);
+	}
+	this->parseDatabase(file);
+	return (SUCCESS);
+}
+
+// Checks through every line in the file for the key value pairs, and populates the exchange rate hashmap with them.
+void	BitcoinExchange::parseDatabase( std::ifstream& databaseFile )
+{
+	std::string	line;
+	std::string	date;
+	double		rateValue;
+
+	while (std::getline(databaseFile, line))
+	{
+		if (line != "date,exchange_rate")
+		{
+			std::istringstream ss(line);
+			if (std::getline(ss, date, ',') && (ss >> rateValue))
+				this->_exchangeRates[date] = rateValue;	
+		}
+	}
+}
+
+// Loads input variables
+int		BitcoinExchange::loadInput( void )
+{
+	std::ifstream file;
+
+	file.open(this->getInputName());
+	if (!file.is_open())
+	{
+		std::cerr << RED << "Error: Could not open input file with path: " << RESET << this->getInputName() << std::endl;
+		return (ERROR);
+	}
+	this->parseInput(file);
+	return (SUCCESS)
+}
+
+static bool isNotSpace(char c)
+{
+	return (!std::isspace(static_cast<unsigned char>(c)));
+}
+
+static std::string& rtrim(std::string& s)
+{
+	s.erase(std::find_if(s.rbegin(), s.rend(), isNotSpace).base(), s.end());
+	return (s);
+}
+
+static std::string& ltrim(std::string& s)
+{
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), isNotSpace));
+	return (s)
+}
+
+static std::string& trim( std::string& s )
+{
+	return (ltrim(rtrim(s)));
+}
+
+void	BitcoinExchange::parseInput( std::ifstream& file )
+{
+	std::string	line;
+	std::string	date;
+	double		rateValue;
+
+	while (std::getline(file, line))
+	{
+		trim(line);
+		if 
+	}
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ilevy <ilevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 15:18:00 by ilevy             #+#    #+#             */
-/*   Updated: 2025/06/27 15:26:52 by ilevy            ###   ########.fr       */
+/*   Updated: 2025/07/07 09:52:03 by ilevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,59 @@
 
 # include <iostream>
 # include <map>
+# include <fstream>
+# include <sstream>
+# include <cstdlib>
+# include <algorithm>
 
-// This exercise, I've made the choice to use a hashmap in order to make full use of the key/value pair
-// that it provides.
-// Indeed, we need to find the value of the bitcoin at a specific date, hence date as a string and value as a float.
+# define SUCCESS 0
+# define ERROR 1
+# define CRITICAL_ERROR -1
+
+
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define RED "\033[31m"
+# define CYAN "\033[36m"
+# define MAGENTA "\033[35m"
+# define BLUE "\033[34m"
+# define RESET "\033[0m"
+
+
+// This exercise, I've made the choice to use a hashmap in order to make full use of the key/value pair that it provides.
+// Indeed, we need to find the value of the bitcoin at a specific date, hence date as a string and value as a double.
 
 class BitcoinExchange
 {
 	private:
-		std::map<std::string, float> _map;
+	
+		std::string							_inputName;
+		std::map<std::string, double>		_exchangeRates;
+		
+		BitcoinExchange( void );
+
+		//Will parse the Database and put the exchange rate in the map.
+		void		parseDatabase( std::ifstream& databaseFile );
+		void		parseInput( std::ifstream& inputFile );
+		bool		isValidDate( const std::string& date );
+		bool		isValidValue( const std::string& value );
 	public:
 	
-		//
-		BitcoinExchange( void );
+		// Constructor and destructors
+		BitcoinExchange( const std::string& inputName );
 		BitcoinExchange( const BitcoinExchange& copy );
 		BitcoinExchange& operator=( const BitcoinExchange& src );
 		~BitcoinExchange( void );
+
+		//Getters
+		const std::string&						getInputName( void ) const;
+		const std::map<std::string, double>&	getExchangeRates( void ) const;
+		
+		//Will be called with database name, and populates exchange rate hashmap.
+		int		loadDatabase( const std::string& database );
+		
+		//Loads input received at construction in _inputName
+		int		loadInput( void );
 	
 };
 
